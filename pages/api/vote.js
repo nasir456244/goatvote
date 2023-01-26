2312dfc66beedf2bc32e850057e10cb446e4f8c8
nasir456244 <arturlegendflame@gmail.com>
2023-01-26 14:38:19 +1000

    validate api

--- pages/api/vote.js
+++ pages/api/vote.js
@@ -2,12 +2,20 @@ import { admin, db } from '../../lib/firebase-admin'
 export default async function handler(req, res) {
 
     try {
-    const { vote } = req?.body
+      if(req.method !== "POST") {
+        res.status(405).json({message: "Wrong method"})
+        return
+      }
+      const { vote } = req?.body
+      if(!vote || vote === "") {
+        res.status(400).json({message: "you need to provide who you are voting for"})
+        return
 
-    await db.collection("goat").doc("goatvote").update({[`${vote}.votes`]: admin.firestore.FieldValue.increment(1)
-})
-    
+      }
+      await db.collection("goat").doc("goatvote").update({[`${vote}.votes`]: admin.firestore.FieldValue.increment(1)
+      })
     res.status(200).json({ message: "Vote submitted"})
+    
   }
   catch (err) {
     res.status(500).json({err})
